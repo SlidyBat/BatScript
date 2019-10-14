4a63bdf865bdb00c5fd1c60498147e384bfd64d7
SlidyBat <25954912+SlidyBat@users.noreply.github.com>
2019-10-14 18:46:52 +1100

    Add github actions workflow for building/testing

--- BatScript/test2.bat
+++ BatScript/test2.bat
@@ -1,13 +1,69 @@
-var g = 0
+print 10 + 5
+print 10.0 + 5.1
+print 10 - 5
+print 10.0 - 5.1
+print 10 / 5
+print 10 / 7
+print 10.0 / 7
+print 10 * 5
+print 10.0 * 5.1
+print 1 << 1
+print 4 >> 1
+print 5 % 2
+print ~(-1)
+print 2 ^ 1
+print 4 | 1
+print 3 & 1
+print !true
+print !false
+print !1
+print !0
+print 10 < 5
+print 10 < 20
+print 10 > 5
+print 10 > 20
+print 10.0 < 5.0
+print 10.0 < 20.0
+print 10.0 > 5
+print 10.0 > 20.0
+print 10 <= 10
+print 10 <= 5
+print 10 <= 20
+print 10.0 <= 10.0
+print 10.0 <= 5.0
+print 10.0 <= 20.0
+print 10 >= 10
+print 10 >= 5
+print 10 >= 20
+print 10.0 >= 10.0
+print 10.0 >= 5.0
+print 10.0 >= 20.0
+print 10 == 5
+print 10 == 10
+print 10 == 20
+print 10.0 == 5.0
+print 10.0 == 10.0
+print 10.0 == 20.0
 
-void test
 
-def f():
-	g += 1
-	return g
-
-def foo(int a, int b):
-	print a
-	print b
-
-foo(f(), f())
+int a = 5
+print a
+int b = 10
+print b
+b += a
+print b
+b -= a
+print b
+b /= a
+print b
+b *= a
+print b
+b %= a
+print b
+b |= a
+print b
+b ^= a
+print b
+b = 10
+b ^= a
+print b
--- BatScript/tests/run_tests.py
+++ BatScript/tests/run_tests.py
@@ -3,24 +3,30 @@ import sys
 import subprocess
 import argparse
 
-def get_tests_impl(tests, root, relroot):
+def get_tests_impl(tests, test_paths, root, relroot):
     for filename in os.listdir(root):
         abspath = os.path.join(root, filename)
         relpath = os.path.join(relroot, filename)
         if os.path.isdir(abspath):
-            get_tests_impl(tests, abspath, relpath)
+            get_tests_impl(tests, test_paths, abspath, relpath)
         else:
-            base, ext = os.path.splitext(relpath)
+            base, ext = os.path.splitext(abspath)
             if ext == '.bat':
-                tests += [base]
+                test_paths += [base]
+            relbase, relext = os.path.splitext(relpath)
+            if relext == '.bat':
+                tests += [relbase]
+            
 
 def get_tests():
     tests = []
-    get_tests_impl(tests, os.path.dirname(os.path.abspath(__file__)), '')
-    return tests
+    test_paths = []
+    get_tests_impl(tests, test_paths, os.path.dirname(os.path.abspath(__file__)), '')
+    return tests, test_paths
 
-def run_tests(tests, method=None):
-    for test in tests:
+def run_tests(tests, test_paths, method=None, compiler_path=None):
+    all_passed = True
+    for test, test_path in zip(tests, test_paths):
         test_name = os.path.basename(test)
         
         if 'ok-' in test_name:
@@ -32,28 +38,37 @@ def run_tests(tests, method=None):
             continue
         
         try:
-            argv = ['BatScript.exe', test + '.bat']
+            argv = [compiler_path, test_path + '.bat']
             if method != None:
                 argv += ['--method', method]
             p = subprocess.Popen(argv, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
             stdout, stderr = p.communicate()
             out = stdout if kind == 'ok' else stderr
 
-            if not os.path.exists(test + '.out'):
+            if not os.path.exists(test_path + '.out'):
                 print('No expected result file for %s, creating one ...' % test)
-                with open(test + '.out', 'w') as f:
+                with open(test_path + '.out', 'w') as f:
                     f.write(out)
             else:
-                with open(test + '.out', 'r') as f:
+                with open(test_path + '.out', 'r') as f:
                     expected = f.read()
                 
                 failed = False
                 if kind == 'ok' and expected != stdout:
                     failed = True
-                elif kind == 'fail' and expected != stderr:
-                    failed = True
+                elif kind == 'fail':
+                    expected_lines = expected.split('\n')
+                    actual_lines = [line.split('Error: ')[-1] for line in stderr.split('\n')]
+                    for i in range(len(expected_lines)):
+                        if i >= len(actual_lines):
+                            failed = True
+                            break
+                        if not actual_lines[i] in expected_lines[i]:
+                            failed = True
+                            break
                 
                 if failed:
+                    all_passed = False
                     print('Test %s ... FAIL' % test)
                     print('Dumping output...')
                     print(out)
@@ -62,14 +77,20 @@ def run_tests(tests, method=None):
         except Exception as e:
             raise
             sys.stderr.write('Failed! %s' % e.message)
+    return all_passed
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--method', type=str, default='vm')
+    parser.add_argument('--compiler', type=str, default='BatScript.exe')
     args = parser.parse_args()
 
-    tests = get_tests()
-    run_tests(tests, method=args.method)
+    tests, test_paths = get_tests()
+    all_passed = run_tests(tests, test_paths, compiler_path=args.compiler, method=args.method)
+    if all_passed:
+        sys.exit(0)
+    else:
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
